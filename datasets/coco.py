6fc66f677c53e72a0ed8bee00dee026f7dd91b70
borisdavid98 <71281941+borisdavid98@users.noreply.github.com>
2020-12-15 19:05:06 +0100

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -140,6 +140,12 @@ def make_coco_transforms(image_set):
             T.RandomResize([800], max_size=1333),
             normalize,
         ])
+        
+    if image_set == 'validation':
+        return T.Compose([
+            T.RandomResize([800], max_size=1333),
+            normalize,
+        ])
 
     raise ValueError(f'unknown {image_set}')
 
@@ -148,11 +154,17 @@ def build(image_set, args):
     root = Path(args.coco_path)
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
-    PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
-    }
-
+    if args.validation:
+		PATHS = {
+			"train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
+			"val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+			"validation": (root / "validation2017", root / "annotations" / f'{mode}_validation2017.json')
+		}
+	else:
+		PATHS = {
+			"train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
+			"val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json')
+		}
     img_folder, ann_file = PATHS[image_set]
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
     return dataset
